cfbba7d1c3350d1cc8dd4d705a0d61ba628ab9a9
flowyan <71491435+antoninvondrovic@users.noreply.github.com>
2022-05-27 14:52:42 +0200

    dont need module

--- jsonParser/app.py
+++ jsonParser/app.py
@@ -5,5 +5,5 @@ class JSONParserApp(bspump.BSPumpApplication):
     def __init__(self):
         super().__init__()
 
-        from .module import ParserModule
-        self.add_module(ParserModule)
+        from .service import ParserService
+        self.ParserService = ParserService(self)
