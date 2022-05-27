7b6c32588bea81a7dba936a42a06a5d79d619eb5
Antonín Vondrovic <71491435+antoninvondrovic@users.noreply.github.com>
2022-05-27 15:21:32 +0200

    made the ifs "better"

--- jsonParser/processor.py
+++ jsonParser/processor.py
@@ -16,30 +16,27 @@ class ParserProcessor(bspump.Processor):
         super().__init__(app, pipeline)
 
     def process(self, ctx, e):
+        cpu_usage = None
+        memory_usage = None
+        addresses = None
+        created_at = None
+
         name = e.get('name')
 
         if e.get('state') is not None:
             if e.get('state').get('cpu') is not None: cpu_usage = e.get('state').get('cpu').get('usage')
-            else: cpu_usage = None
             if e.get('state').get('memory') is not None: memory_usage = e.get('state').get('memory').get('usage')
-            else: memory_usage = None
 
             # shows all addresses from eth0 (ethernet port)
             if e.get('state').get('network') is not None:
                 if e.get('state').get('network').get('eth0') is not None: addresses = e.get('state').get('network').get('eth0').get('addresses')
-                else: addresses = None
-            else: addresses = None
-
-        else: cpu_usage = None; memory_usage = None; addresses = None
 
         # conversion to UTC timestamp
         if e.get('created_at') is not None:
             parsed_time = datetime.strptime(e.get('created_at'), "%Y-%m-%dT%H:%M:%S%z")
             created_at = int(datetime.utcfromtimestamp(parsed_time.timestamp()).timestamp())
-        else: created_at = None
 
         status = e.get('status')
-
 
         parsed_json = {
             'name': name,
